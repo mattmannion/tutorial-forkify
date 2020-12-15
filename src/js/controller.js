b2767c3e7b2da3ed3da499c0a7ce4b4ad452e91d
Matt Mannion <mattpmannion@gmail.com>
2020-12-15 16:04:30 -0500

    inital commit

---
+++ src/js/controller.js
@@ -0,0 +1,50 @@
+import * as model from './model';
+import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultsView from './views/resultsView';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+const controlRecipe = async function () {
+    try {
+        const id = window.location.hash.slice(1);
+
+        if (!id) return;
+        recipeView.renderSpinner();
+
+        //Loading recipe
+        await model.loadRecipe(id);
+
+        // rendering recipe
+        recipeView.render(model.state.recipe);
+    } catch (err) {
+        recipeView.renderError();
+    }
+};
+
+const controlSearchResults = async function () {
+    try {
+        resultsView.renderSpinner();
+
+        const query = searchView.getQuery();
+
+        if (!query) return;
+
+        await model.loadSearchResults(query);
+
+        resultsView.render(model.state.search.results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const init = function () {
+    recipeView.addHandlerRender(controlRecipe);
+    searchView.addHandlerSearch(controlSearchResults);
+};
+
+init();
